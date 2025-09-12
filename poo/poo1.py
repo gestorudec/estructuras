437becbecaf25e46e32f03823edefc84522eec84
padillasam <panarick@gmail.com>
2025-09-12 16:35:14 -0500

    Continuación Programación Orientada a Objetos

--- poo/poo1.py
+++ poo/poo1.py
@@ -1,11 +1,24 @@
 class Persona:     
        
     def __init__(self,nombre,documento):
-        self._nombre=nombre
-        self._documento=documento
+        self.__nombre=nombre
+        self.__documento=documento
     def datos(self):
-        return f"{self.nombre} {self.documento}"
+        return f"{self.__nombre} {self.__documento}"
 
 objeto=Persona("Clara",123)
-objeto._nombre="luz"
-print(objeto._nombre)
+objeto.__nombre="luz"
+print(objeto.__nombre)
+print(objeto.datos())
+
+#HERENCIA EN P.O.O CON PYTHON
+class Estudiante(Persona):
+    def __init__(self, nombre, documento,carrera):
+        super().__init__(nombre, documento)
+        self.__carrera=carrera
+    def datos(self):
+        return super().datos()+" "+self.__carrera
+
+
+e1=Estudiante("Mary",333,"Sistemas")
+print(e1.datos())
