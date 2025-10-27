9eb65cdf5be118a8f6e20945656633f787228a0f
padillasam <panarick@gmail.com>
2025-10-27 16:20:43 -0500

    TDA colas

---
+++ pilas_y_colas/colas1.py
@@ -0,0 +1,17 @@
+elementos=[]
+elementos.append("Luis")
+print(elementos)
+elementos.append("Ernesto")
+print(elementos)
+elementos.append("Sandra")
+print(elementos)
+elementos.append("Lucia")
+print(elementos)
+elementos.append("Lucas")
+print(elementos)
+elementos.pop(0)
+print(elementos)
+while len(elementos)!=0:
+    elementos.pop(0)
+    print(elementos)
+    
